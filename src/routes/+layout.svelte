9712581744dbd2c7f06f084dbb948382f1aa03d7
Brage Sekse Aarset <brage@bjerk.io>
2023-12-10 14:38:53 +0200

    feat: add layout and improved navigation

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<svelte:head>
+	<title>Hacklab</title>
+</svelte:head>
+<slot />
+<div>
+	{#if data.loggedIn}
+		<p>You are logged in as {data?.username}</p>
+		{#if $page.url.pathname !== '/profile'}
+			<a href="/profile">Go to Profile</a>
+		{/if}
+		<a href="/logout">Log Out</a>
+	{:else}
+		<p>You are not logged in</p>
+		<a href="/login">Log In</a>
+		<a href="/signup">Sign Up</a>
+	{/if}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,1 @@
-<script lang="ts">
-	import type { PageData } from './$types';
-
-	export let data: PageData;
-</script>
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-{#if data.loggedIn}
-	<p>You are logged in as {data?.username}</p>
-	<a href="/logout">Log Out</a>
-{:else}
-	<p>You are not logged in</p>
-	<a href="/login">Log In</a>
-	<a href="/signup">Sign Up</a>
-{/if}
+<h1>Welcome to Hacklab</h1>
